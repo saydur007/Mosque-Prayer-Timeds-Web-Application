76650857e3006f65a84dedc1bd17e8e8a94be563
Saydur Rahman <rahmansaydur511@gmail.com>
2024-12-25 11:51:19 -0500

    added animations

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,7 +3,27 @@ import '../styles/HomePage.css';
 import dclogo from './dc.png';
 import usePrayerTimes from '../functions/usePrayerTimes';
 function HomePage({ prayerTimes, changedprayer }) {
- /// const { blink, darken, message, countdown } = usePrayerTimes(prayerTimes);
+  const { blinkFajr,
+    darkenFajr,
+    blinkZuhr,
+    darkenZuhr,
+    blinkAsr,
+    darkenAsr,
+    blinkMaghrib,
+    darkenMaghrib,
+    blinkIsha,
+    darkenIsha,
+    message,
+    countdown} = usePrayerTimes(prayerTimes);
+ const [showJummah, setShowJummah] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowJummah(prevShowJummah => !prevShowJummah);
+    }, 5000); // Switch every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className={`home-page`}>
       <div>
@@ -11,7 +31,7 @@ function HomePage({ prayerTimes, changedprayer }) {
         <img src={dclogo}></img>
       </div>
       </div>
-    
+      {message && <p className="message blinktext">{message}</p>}
       <table id="prayerTimesTable"  >
     <thead>
       <tr>
@@ -24,28 +44,28 @@ function HomePage({ prayerTimes, changedprayer }) {
 
       <tr>
         <td id = "prayerName">Fajr</td>
-        <td >{prayerTimes.Fajr} am</td>
+        <td className={`${blinkFajr ? 'blink' : ''} ${darkenFajr ? 'darken' : ''}`}>{prayerTimes.Fajr} am</td>
         <td>{changedprayer.Fajr} am</td>
       </tr>
 
       <tr>
       <td id = "prayerName">Zuhr</td>
-        <td >{prayerTimes.Zuhr} pm</td>
+        <td className={`${blinkZuhr ? 'blink' : ''} ${darkenZuhr ? 'darken' : ''}`}>{prayerTimes.Zuhr} pm</td>
         <td>{changedprayer.Zuhr} pm </td>
       </tr>
       <tr>
       <td id = "prayerName">Asr</td>
-        <td >{prayerTimes.Asr} pm</td>
+        <td className={`${blinkAsr ? 'blink' : ''} ${darkenAsr ? 'darken' : ''}`}>{prayerTimes.Asr} pm</td>
         <td>{changedprayer.Asr} pm</td>
       </tr>
       <tr>
       <td id = "prayerName">Maghrib</td>
-        <td >{prayerTimes.Maghrib} pm </td>
+        <td className={`${blinkMaghrib ? 'blink' : ''} ${darkenMaghrib ? 'darken' : ''}`}>{prayerTimes.Maghrib} pm </td>
         <td>{changedprayer.Maghrib} pm </td>
       </tr>
       <tr>
       <td id = "prayerName">Isha</td>
-        <td >{prayerTimes.Isha} pm </td>
+        <td className={`${blinkIsha ? 'blink' : ''} ${darkenIsha ? 'darken' : ''}`}>{prayerTimes.Isha} pm </td>
         <td>{changedprayer.Isha} pm </td>
       </tr>
 
@@ -53,26 +73,58 @@ function HomePage({ prayerTimes, changedprayer }) {
 
   </table>
   <br></br>
-  <table id="prayerTimesTable"  >
-    <thead>
-      <tr>
-        <th>Jummah Prayers</th>
-        <th>1st Prayer</th>
-        <th>2nd Prayer</th> 
-      </tr>
-    </thead>
-    <tbody>
-
-      <tr>
-        <td id = "prayerName">Khutbah</td>
-        <td> 12:15 pm</td>
-        <td>1:15 pm</td>
-      </tr>
-
-
-    </tbody>
-  </table>
-
+  <table id="prayerTimesTable">
+          <thead>
+            <tr>
+              <th>Jummah Prayers</th>
+              <th>1st Prayer</th>
+              <th>2nd Prayer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td id="prayerName">Khutbah</td>
+              <td>12:15 pm</td>
+              <td>1:15 pm</td>
+            </tr>
+          </tbody>
+        </table>
+  {/* {showJummah ? (
+        <table id="prayerTimesTable">
+          <thead>
+            <tr>
+              <th>Jummah Prayers</th>
+              <th>1st Prayer</th>
+              <th>2nd Prayer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td id="prayerName">Khutbah</td>
+              <td>12:15 pm</td>
+              <td>1:15 pm</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <table id="prayerTimesTable">
+          <thead>
+            <tr>
+              <th>EID Prayers</th>
+              <th>1st Prayer</th>
+              <th>2nd Prayer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td id="prayerName">Khutbah</td>
+              <td>12:15 pm</td>
+              <td>1:15 pm</td>
+            </tr>
+          </tbody>
+        </table>
+      )} */}
+ 
     </div>
 
   );
--- frontend/src/functions/usePrayerTimes.js
+++ frontend/src/functions/usePrayerTimes.js
@@ -1,91 +1,157 @@
-import axios from 'axios';
-import { calculateFajrAsr, calculateIsha, calculateZuhr, calculateMaghrib } from './calculation';
+import { useState, useEffect } from 'react';
 
-const InsertYear = async (year) => {
-  const API_URL = `https://api.aladhan.com/v1/calendarByCity/${year}?city=Toronto&country=Canada&method=15`;
 
-  try {
-    const response = await axios.get(API_URL); // Replace with your actual API endpoint
-    console.log('API Response:', response); // Debugging statement
+const convertTo24Hour = (time) => {
+  let [hours, minutes] = time.split(':');
 
-    // Check if response.data and response.data.data exist
-    if (!response.data || !response.data.data) {
-      throw new Error('Invalid API response structure');
-    }
-
-    const entireData = response.data.data;
-    console.log('Entire Data:', entireData); // Debugging statement
-
-    var Fajr = "";
-    var Asr = "";
-    var Zuhr = "";
-    var Maghrib = "";
-    var Isha = "";
-
-    for (const months in entireData) {
-      const datasets = entireData[months];
-      for (const dataset of datasets) {
-        const day = dataset.date.gregorian.day;
-        const month = dataset.date.gregorian.month.number;
-        const year = dataset.date.gregorian.year;
-        const date = `${year}-${month}-${day}`;
-        const weekday = dataset.date.gregorian.weekday.en;
-
-        if (
-          (weekday === "Saturday" && month === 3 && day >= 7 && day <= 13) ||
-          (weekday === "Saturday" && month === 11 && day >= 1 && day <= 6) ||
-          (weekday === "Saturday" && month === 10 && day === 31)
-        ) {
-          // Do nothing
-        } else if (weekday === "Saturday") {
-          Fajr = calculateFajrAsr(dataset.timings.Fajr);
-          Asr = calculateFajrAsr(dataset.timings.Asr);
-          Zuhr = calculateZuhr(dataset.timings.Dhuhr);
-          Maghrib = calculateMaghrib(dataset.timings.Maghrib);
-          Isha = calculateIsha(dataset.timings.Isha);
-        } else if (weekday === "Sunday" && month === 3 && day >= 8 && day <= 14) {
-          Fajr = calculateFajrAsr(dataset.timings.Fajr);
-          Asr = calculateFajrAsr(dataset.timings.Asr);
-          Zuhr = calculateZuhr(dataset.timings.Dhuhr);
-          Maghrib = calculateMaghrib(dataset.timings.Maghrib);
-          Isha = calculateIsha(dataset.timings.Isha);
-        } else if (weekday === "Sunday" && month === 11 && day >= 1 && day <= 7) {
-          Fajr = calculateFajrAsr(dataset.timings.Fajr);
-          Asr = calculateFajrAsr(dataset.timings.Asr);
-          Zuhr = calculateZuhr(dataset.timings.Dhuhr);
-          Maghrib = calculateMaghrib(dataset.timings.Maghrib);
-          Isha = calculateIsha(dataset.timings.Isha);
-        } else {
-          Maghrib = calculateMaghrib(dataset.timings.Maghrib);
-        }
-
-        const FajrStart = dataset.timings.Fajr;
-        const ZuhrStart = dataset.timings.Dhuhr;
-        const AsrStart = dataset.timings.Asr;
-        const IshaStart = dataset.timings.Isha;
-        const timingswithDate = { date, weekday, FajrStart, Fajr, ZuhrStart, Zuhr, AsrStart, Asr, Maghrib, IshaStart, Isha };
-
-        try {
-          const existingTiming = await axios.get(`http://tests-env-1.eba-dudpkeyx.us-east-1.elasticbeanstalk.com/prayerTimes/${date}`);
-          if (!existingTiming.data) {
-            // Insert a new record
-            await axios.post('http://tests-env-1.eba-dudpkeyx.us-east-1.elasticbeanstalk.com/prayerTimes', timingswithDate);
-            console.log('Data successfully inserted into the database');
-          }
-        } catch (error) {
-          if (error.response && error.response.status === 404) {
-            // Insert a new record if not found
-            await axios.post('http://tests-env-1.eba-dudpkeyx.us-east-1.elasticbeanstalk.com/prayerTimes', timingswithDate);
-            console.log('Data successfully inserted into the database');
-          } else {
-            console.error('Error inserting prayer time:', error.response ? error.response.data : error);
-          }
-        }
-      }
-    }
-  } catch (error) {
-    console.error('Error fetching prayer times:', error);
+  if (hours === '12') {
+    hours = '00';
   }
+    hours = parseInt(hours, 10) + 12;
+  
+
+  return `${hours}:${minutes}`;
+};
+const usePrayerTimes = (prayerTimes) => {
+  const prayerTimess = {
+    Fajr: prayerTimes.Fajr,
+    Zuhr: prayerTimes.Zuhr,
+    Asr: prayerTimes.Asr,
+    Maghrib: prayerTimes.Maghrib, // Replace with actual time if available
+    Isha: prayerTimes.Isha,
+  };
+  const [blinkFajr, setBlinkFajr] = useState(false);
+  const [darkenFajr, setDarkenFajr] = useState(false);
+  const [blinkZuhr, setBlinkZuhr] = useState(false);
+  const [darkenZuhr, setDarkenZuhr] = useState(false);
+  const [blinkAsr, setBlinkAsr] = useState(false);
+  const [darkenAsr, setDarkenAsr] = useState(false);
+  const [blinkMaghrib, setBlinkMaghrib] = useState(false);
+  const [darkenMaghrib, setDarkenMaghrib] = useState(false);
+  const [blinkIsha, setBlinkIsha] = useState(false);
+  const [darkenIsha, setDarkenIsha] = useState(false);
+  const [message, setMessage] = useState("");
+  const [countdown, setCountdown] = useState({});
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = new Date();
+      const currentMinutes = currentTime.getHours() * 60 + currentTime.getMinutes();
+      const currentSeconds = currentMinutes * 60 + currentTime.getSeconds();
+
+      Object.keys(prayerTimess).forEach(prayer => {
+       
+        var timeString = "";
+        if (prayer !== "Fajr"){
+         timeString = convertTo24Hour(prayerTimess[prayer]);
+        }
+        else{
+           timeString = prayerTimess[prayer];
+        }
+        
+        const [hours, minutes] = timeString.split(":").map(Number);
+        const prayerMinutes = hours * 60 + minutes;
+        const prayerSeconds = prayerMinutes * 60;
+
+        if (currentSeconds >= prayerSeconds + 120) {
+          setBlink(prayer, false);
+          setDarken(prayer, false);
+          setMessage("");
+        } else if (currentSeconds >= prayerSeconds) {
+          setBlink(prayer, true);
+          setDarken(prayer, true);
+          setMessage("It is Time for Salah");
+        } else if (currentSeconds >= prayerSeconds - 600) {
+          setBlink(prayer, false);
+          setDarken(prayer, false);
+
+          // Calculate the countdown
+          const remainingSeconds = prayerSeconds - currentSeconds;
+          const minutesRemaining = Math.floor(remainingSeconds / 60);
+          const secondsRemaining = remainingSeconds % 60;
+          const countdownString = `${minutesRemaining}m ${secondsRemaining}s`;
+
+          setMessage(`Prayer soon: ${countdownString}`);
+          setCountdown(prev => ({
+            ...prev,
+            [prayer]: countdownString,
+          }));
+        } else if (currentSeconds >= prayerSeconds - 900) {
+          setBlink(prayer, true);
+          setDarken(prayer, false);
+          setMessage("Time for Adhan");
+        } else {
+          setBlink(prayer, false);
+          setDarken(prayer, false);
+          setCountdown(prev => ({
+            ...prev,
+            [prayer]: "",
+          }));
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const setBlink = (prayer, value) => {
+    switch (prayer) {
+      case "Fajr":
+        setBlinkFajr(value);
+        break;
+      case "Zuhr":
+        setBlinkZuhr(value);
+        break;
+      case "Asr":
+        setBlinkAsr(value);
+        break;
+      case "Maghrib":
+        setBlinkMaghrib(value);
+        break;
+      case "Isha":
+        setBlinkIsha(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const setDarken = (prayer, value) => {
+    switch (prayer) {
+      case "Fajr":
+        setDarkenFajr(value);
+        break;
+      case "Zuhr":
+        setDarkenZuhr(value);
+        break;
+      case "Asr":
+        setDarkenAsr(value);
+        break;
+      case "Maghrib":
+        setDarkenMaghrib(value);
+        break;
+      case "Isha":
+        setDarkenIsha(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return {
+    blinkFajr,
+    darkenFajr,
+    blinkZuhr,
+    darkenZuhr,
+    blinkAsr,
+    darkenAsr,
+    blinkMaghrib,
+    darkenMaghrib,
+    blinkIsha,
+    darkenIsha,
+    message,
+    countdown,
+  };
 };
 
-export default InsertYear;
+export default usePrayerTimes;
